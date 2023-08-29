b3af8ccb05f7e670b633c238058218d4226e25f3
Max Korpinen <maxkorpinen@gmail.com>
2023-08-29 18:41:25 +0300

    improved data model, added real data

--- aoe-back/index.js
+++ aoe-back/index.js
@@ -49,67 +49,87 @@ civs = ['Aztecs',
 const civilizations = [
   {
     id: 1,
-    name: 'britons',
+    name: 'Britons',
     units: [
-      {militia: 3,
-      spearman: 3,
-      skirmisher: 3,
-      archer: 5,
-      carcher: 3,
-      scout: 3,
-      knight: 3}
+      {
+        militia: 5,
+        spearman: 5,
+        eagle: null,
+        scout: 4,
+        knight: 4,
+        camel: null,
+        archer: 7,
+        skirmisher: 5,
+        carcher: 3
+      }
     ]
   },
-  { 
+  {
     id: 2,
-    name: 'franks',
+    name: 'Franks',
     units: [
-      {militia: 3,
-      spearman: 3,
-      skirmisher: 3,
-      archer: 5,
-      carcher: 3,
-      scout: 3,
-      knight: 3}
+      {
+        militia: 5,
+        spearman: 5,
+        eagle: null,
+        scout: 5,
+        knight: 6,
+        camel: null,
+        archer: 4,
+        skirmisher: 5,
+        carcher: 3
+      }
     ]
   },
-  { 
+  {
     id: 3,
-    name: 'mongols',
+    name: 'Goths',
     units: [
-      {militia: 3,
-      spearman: 3,
-      skirmisher: 3,
-      archer: 5,
-      carcher: 3,
-      scout: 3,
-      knight: 3}
+      {
+        militia: 7,
+        spearman: 7,
+        eagle: null,
+        scout: 5,
+        knight: 5,
+        camel: null,
+        archer: 4,
+        skirmisher: 5,
+        carcher: 3
+      }
     ]
   },
-  { 
+  {
     id: 4,
-    name: 'mayans',
+    name: 'Mayans',
     units: [
-      {militia: 3,
-      spearman: 3,
-      skirmisher: 3,
-      archer: 5,
-      carcher: 3,
-      scout: 3,
-      knight: 3}
+      {
+        militia: 4,
+        spearman: 5,
+        eagle: 5,
+        scout: null,
+        knight: null,
+        camel: null,
+        archer: 6,
+        skirmisher: 5,
+        carcher: null
+      }
     ]
   },
-  { 
+  {
     id: 5,
-    name: 'goths',
+    name: 'Mongols',
     units: [
-      {militia: 3,
-      spearman: 3,
-      skirmisher: 3,
-      archer: 5,
-      carcher: 3,
-      scout: 3,
-      knight: 3}
+      {
+        militia: 4,
+        spearman: 5,
+        eagle: null,
+        scout: 6,
+        knight: 5,
+        camel: 5,
+        archer: 5,
+        skirmisher: 5,
+        carcher: 6
+      }
     ]
   }
 ]
