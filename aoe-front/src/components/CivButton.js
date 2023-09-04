33ca83353f72849d7051c7f48b83fbf6a5ddf0bb
Sampo Hakola <sampo.hakola@helsinki.fi>
2023-09-04 21:59:01 +0300

    added cypress, + first test

--- aoe-front/src/components/CivButton.js
+++ aoe-front/src/components/CivButton.js
@@ -1,12 +1,14 @@
 const CivButton = (props) => {
   const {image, name, buttFunc} = props
+  console.log("name:", name.trim())
   return (
     <button key={name}>
       <img src={image}
       alt = {name}
       onClick={()=> buttFunc(name)}
       width={250}
-      height={250}/>
+      height={250}
+      data-cy={name.trim()}/>
     </button>
   )
 }
--- aoe-front/src/utils/apiClient.js
+++ aoe-front/src/utils/apiClient.js
@@ -11,6 +11,8 @@ const BACKEND = process.env.NODE_ENV === 'development'
   ? process.env.REACT_APP_BACKEND_URL_DEV
   : ''
 
+console.log("config node.env", process.env.NODE_ENV)
+
 const apiClient = axios.create({
   baseURL: BACKEND
 })
